06e3237d07f0655be635894ff7dc19b469d9c8f9
Lanre <lanrefademi@gmail.com>
2023-01-21 20:49:27 -0500

    first commit

---
+++ GCP/auth_with_service_account/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.48.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-gcp-374715"
+  region = "us-central1"
+  zone = "us-central1-a"
+
+} 
+
+resource google_storage_bucket "GCS1"{
+  name = "bucket-from-tf-lafad"
+  location = "US"
+
+}
